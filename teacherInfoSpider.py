b3bd6942d1fae03d4625d244c891e701b0747e93
Giorgio <84867579@qq.com>
2020-09-16 11:17:50 +0800

    Create teacherInfoSpider.py

---
+++ teacherInfoSpider.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import requests
+from bs4 import BeautifulSoup
+f = open('result.txt','w')
+url = 'http://faculty.bjtu.edu.cn/'
+for i in range(1,10000):
+    strhtml = requests.get(url+str(i))
+    if strhtml.status_code == 404:
+        continue
+    soup=BeautifulSoup(strhtml.text,'lxml')
+    data1 = soup.select('div.content1>h1')
+    data2 = soup.select('div.main_left>div.mainleft_box')
+    if len(data1)>0:
+        f.write(data1[0].get_text())
+    f.write('    '+url+str(i))    
+    first = True
+    for i in data2:
+        if i.get_text().find('研究方向') != -1:
+            if first:
+                if len(data1)>0:
+                    f.write(data1[0].get_text()+'    '+url+str(i))
+                    first = False
+            f.write(i.get_text())
+    f.write('\n\n\n')
+print('finish')
+
